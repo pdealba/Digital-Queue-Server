745e6a1e65388ef266c099b7a27514868e8c36f6
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-12-16 11:48:54 -0300

    small changes to the names of the keys and modified timeDelay type from Date to Number

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, require: true },
   email: { type: String, require: true },
-  userPhoneNumber: { type: Number, require: true },
-  waitTime: { type: Date, require: true },
+  PhoneNumber: { type: Number, require: true },
+  timeDelay: { type: Number, require: true },
 });
 
 mongoose.Model("User", userSchema);
